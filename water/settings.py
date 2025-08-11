552703aed5baaee0b9f1e11d41e01a03284b8815
Kavish Malik <kavishmalik@Kavishs-MacBook-Air.local>
2025-08-11 21:54:17 +0530

    Update .gitignore

--- water/settings.py
+++ water/settings.py
@@ -17,7 +17,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key-if-not-set')
 
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+
 
 # Installed apps
 INSTALLED_APPS = [
